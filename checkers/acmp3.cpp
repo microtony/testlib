af9499707d228ae5fc0a0d788b38194f1a4f79d9
Tony Wong <microtony@gmail.com>
2015-09-25 22:07:35 +0800

    Fix acmp3

--- checkers/acmp3.cpp
+++ checkers/acmp3.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-const double EPS = 0.00101;
+const double EPS = 0.0001;
 
 int main(int argc, char * argv[])
 {
-    setName("compare two sequences of doubles, max absolute error = %.5f", EPS);
+    setName("compare two sequences of doubles, max absolute error = 0.000",);
     registerTestlibCmd(argc, argv);
 
     int n = 0;
@@ -20,13 +20,13 @@ int main(int argc, char * argv[])
         p = ouf.readDouble();
         if (fabs(j - p) > EPS)
         {
-            quitf(_wa, "%d%s numbers differ - expected: '%.5f', found: '%.5f', error = '%.5f'",
+            quitf(_wa, "%d%s numbers differ - expected: '%.3f', found: '%.3f', error = '%.3f'",
                 n, englishEnding(n).c_str(), j, p, fabs(j - p));
         }
     }
 
     if (n == 1)
-        quitf(_ok, "found '%.5f', expected '%.5f', error '%.5f'", p, j, fabs(j - p));
+        quitf(_ok, "found '%.3f', expected '%.3f', error '%.3f'", p, j, fabs(j - p));
     
     quitf(_ok, "%d numbers", n);
 }
